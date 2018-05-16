fe049dc1218ea51af2895beaef75e04ede9e9d70
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-16 13:53:00 -0400

    Update aiko.js

--- aiko.js
+++ aiko.js
@@ -1,6 +1,7 @@
 var movies = require('./movies');
 //objectFactory
 var aikoMovies = movies();
+aikoMovies.favMovie = "Good Will Hunting";
 console.log("aiko's favorite movie is: " + aikoMovies.favMovie);
 
 //console.log("Emi's favorite movie is : "+ movies.favMovie);
