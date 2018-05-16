6bbbce80d4bb518c108d56df200de98bb5781487
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-16 11:07:45 -0400

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,9 @@
+function printAvatar(){
+  console.log("Avatar: PG-13");
+  }
+function printChappie(){
+  console.log("Chappie: R");
+  }
+  
+//export this specific module and name it as avatar to print out its log
+module.exports.avatar=printAvatar;
