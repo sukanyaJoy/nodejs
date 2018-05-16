433e16f20709f2e7b385a810b3d9e09da2aaee0b
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-16 10:51:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,83 @@
-function placeAnOrder(orderNumber){
-  
+//modules--> working with movies.js
+
+
+
+
+//prototype
+funtion User(){
+  this.name = "";
+  this.life = 100;
+  this.giveLife = functin giveLife(targetPlayer){
+    targetPlayer.life += 1;
+    console.log(this.name+ "gave 1 life to " + targetPlayer.name);
+  }
+}
+var Emi = new User();
+var Wendy = new User();
+
+Emi.name = "Emi";
+Wendy.name = "Wendy";
+
+Emi.giveLife(Wendy);
+console.log("Emi:" +Emi.life);
+console.log("Wedndy:"+Wendy.life);
+//you can add functions to all objects
+User.prototype.uppercut = function uppercut(targetPlayer){
+  targetPlayer.lefe -= 3;
+  console.log(this.name + "just uppercutted "+targetPlayer.name);
+};
+Wendy.uppercut(Emi);
+console.log("Emi: "+Emi.life);
+console.log("Wendy: "+Wendy.life);
+//you can add properties to all objects
+User.prototype.magic = 60;
+console.log(Emi.magic);
+console.log(Wendy.magic);
+
+//////////////////////////////////////////////
+var Emi = {
+  printFirstName: funcion(){
+    console.log("My name is Emi");
+    console.log(this === Emi);
+  }
+};
+Emi.printFirstName();
+
+//The default calling context is global
+function doSomethingWorthless(){
+  console.log("\nI am worthless");
+  console.log(this === global);
+}
+doSomethingWorthless();
+/*run
+My name is Emi
+true
+
+I am worthless
+true
+*/
+
+console.log(19=='19'); //true --> comparing value
+console.log(19==='19'); //false --> comparing data type
+/*run
+true
+false
+*/
+
+//node--> everything is referrent, new object and set there properties
+var Emi = {
+  favFood: "bacon",
+  favMovie: "Chappie"
+};
+
+var Person = Emi;
+Person.favFood = "salad";
+console.log(Emi.favFood);
+/*run
+salad
+*/
+
+function placeAnOrder(orderNumber){  
   console.log("Customer order:", orderNumber);
   cookAndDeliverFood(function(){
     console.log("Delivered food order:", orderNumber)
@@ -14,3 +92,11 @@ function cookAndDeliverFood(callback){
 placeAnOrder(1);
 placeAnOrder(2);
 placeAnOrder(3);
+/* run
+placeAnOrder1
+placeAnOrder2
+placeAnOrder3
+Delivered food order:1
+Delivered food order:2
+Delivered food order:3
+*/
