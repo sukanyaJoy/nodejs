060c2596109fe7dadf36e043dcec42891f579b33
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-16 09:16:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-
+function placeAnOrder(orderNumber){
+  
+  console.log("Customer order:", orderNumber);
+  cookAndDeliverFood(function(){
+    console.log("Delivered food order:", orderNumber)
+  })
+}
+//Simulate a 5 second operation
+function cookAndDeliverFood(callback){
+  setTimeout(callback,5000); //means 5 seconds, after 5 seconds call back
+  //settimeout, schedule to callback
+}
+//Simulate users web request
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
