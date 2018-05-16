1b835275317133725633406178f7bc92f8a31962
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-16 11:40:23 -0400

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,3 +1,14 @@
+module.exports = { //node js created export blank object in the background
+//in the key:value pairs form
+  printAvatar: function(){
+    console.log("Avatar");
+  }, 
+  printChappie: function(){
+    console.log("Chappie");
+  }
+  favMovie: "The Matrix"
+}
+/* basic method to export module 
 function printAvatar(){
   console.log("Avatar: PG-13");
   }
@@ -7,3 +18,5 @@ function printChappie(){
   
 //export this specific module and name it as avatar to print out its log
 module.exports.avatar=printAvatar;
+module.exports.chappie = printChappie;
+*/
