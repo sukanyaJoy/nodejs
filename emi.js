891bc9197fce17d73def1188b67665a68552e816
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-16 13:35:33 -0400

    Update emi.js

--- emi.js
+++ emi.js
@@ -1,3 +1,10 @@
 var movies = require('./movies');
+//objectFactory
+var emiMovies = movies();
+emiMovies.favMovie="The Notebook";
+console.log("Emi's favorite movie is: " + emiMovies.favMovie);
+
+/*10 sharing module
 movies.favMovie = "The Notebook";
 console.log("Emi's favorite movie is : "+ movies.favMovie);
+*/
