01f0219b585bd013f737ca3f038de1f9ebb83cbf
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-16 15:47:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+var http = require('http');
 
+function onRequest(request,response){
+  console.log("A user made a request" + request.url);
+  response.writeHead(200, {"Context-type": "text/plain"});
+  response.write("Here is some data");
+  response.end();
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server is now running....");
+/*
+Server is now running....
+A user made a request/
+A user made a request/favicon.ico
+//when we put localhost:8888 --> show the content of " Here is some data"
+*/
