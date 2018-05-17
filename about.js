f2b73315cebac67750f6bf5d875254420d837183
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-05-17 11:04:22 -0400

    Update about.js

--- about.js
+++ about.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /*GET about page. */
 router.get('/',function (req,res,next){
   res.render('about',{
-    title: 'Express',
+    title: 'About',
     name: 'Emi'
   });
 });
